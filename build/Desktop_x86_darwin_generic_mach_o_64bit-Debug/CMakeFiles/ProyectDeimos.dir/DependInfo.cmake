1c54194237dae4ead9daae188cbac92bac0ad8e4
anisvalle23 <anavalle23@unitec.edu>
2025-05-31 12:32:42 -0600

    clases

---
+++ build/Desktop_x86_darwin_generic_mach_o_64bit-Debug/CMakeFiles/ProyectDeimos.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "ProyectDeimos_autogen/timestamp" "custom" "ProyectDeimos_autogen/deps"
+  "/Users/anavalle/Documents/ProyectoDeimos/build/Desktop_x86_darwin_generic_mach_o_64bit-Debug/ProyectDeimos_autogen/mocs_compilation.cpp" "CMakeFiles/ProyectDeimos.dir/ProyectDeimos_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/ProyectDeimos.dir/ProyectDeimos_autogen/mocs_compilation.cpp.o.d"
+  "/Users/anavalle/Documents/ProyectoDeimos/main.cpp" "CMakeFiles/ProyectDeimos.dir/main.cpp.o" "gcc" "CMakeFiles/ProyectDeimos.dir/main.cpp.o.d"
+  "/Users/anavalle/Documents/ProyectoDeimos/mainwindow.cpp" "CMakeFiles/ProyectDeimos.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/ProyectDeimos.dir/mainwindow.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Desktop_x86_darwin_generic_mach_o_64bit-Debug/CMakeFiles/ProyectDeimos.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProyectDeimos_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/ProyectDeimos_autogen.dir/ParseCache.txt"
+  "ProyectDeimos_autogen"
+  "CMakeFiles/ProyectDeimos.dir/ProyectDeimos_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/ProyectDeimos.dir/ProyectDeimos_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/ProyectDeimos.dir/main.cpp.o"
+  "CMakeFiles/ProyectDeimos.dir/main.cpp.o.d"
+  "CMakeFiles/ProyectDeimos.dir/mainwindow.cpp.o"
+  "CMakeFiles/ProyectDeimos.dir/mainwindow.cpp.o.d"
+  "ProyectDeimos.app/Contents/MacOS/ProyectDeimos"
+  "ProyectDeimos.pdb"
+  "ProyectDeimos_autogen/mocs_compilation.cpp"
+  "ProyectDeimos_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProyectDeimos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
